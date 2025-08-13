ebeb312ac70f83b5f14ef7d1cf18b1d17a5b7ebd
Christian Heel <66922325+heelc29@users.noreply.github.com>
2025-08-13 08:38:37 +0200

    Update Menu.cy.js

--- tests/System/integration/administrator/components/com_menu/Menu.cy.js
+++ tests/System/integration/administrator/components/com_menu/Menu.cy.js
@@ -1,16 +1,20 @@
 describe('Test in backend that the menu form', () => {
   beforeEach(() => cy.doAdministratorLogin());
-  afterEach(() => cy.task('queryDB', "DELETE FROM #__menu_types WHERE menutype = 'test'"));
+  afterEach(() => cy.task('queryDB', "DELETE FROM #__menu_types WHERE menutype LIKE 'test-%-menu'"));
 
-  it('can create a new menu', () => {
+  it('can create a new site menu', () => {
+    cy.visit('/administrator/index.php?option=com_menus&view=menus&filter=&client_id=0');
+    cy.get('#client_id option:selected').should('have.text', 'Site');
     cy.visit('/administrator/index.php?option=com_menus&task=menu.add');
+    cy.get('h1.page-title').should('contain.text', 'Menus: Add');
 
-    cy.get('#jform_title').clear().type('test menu');
-    cy.get('#jform_menutype').clear().type('test');
-    cy.get('#jform_menudescription').clear().type('test description');
+    cy.get('#jform_title').clear().type('test site menu');
+    cy.get('#jform_menutype').clear().type('test-site-menu');
+    cy.get('#jform_menudescription').clear().type('test site menu description');
+    cy.get('#jform_preset').should('not.exist');
     cy.clickToolbarButton('Save & Close');
 
     cy.checkForSystemMessage('Menu saved');
-    cy.contains('test menu');
+    cy.contains('test site menu description');
   });
 });
